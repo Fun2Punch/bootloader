82a6739dcf653593fceb2f14e89e927f84fa1ad8
Admin <dynamicqbit@gmail.com>
2026-01-14 16:28:00 +0900

    paging code modified for simply

--- include/paging.h
+++ include/paging.h
@@ -1,8 +1,22 @@
 #ifndef _PAGING_H
 #define _PAGING_H
 
-BOOLEAN EFIAPI long_mode_enabled(void);
+typedef struct _memory_map {
+  UINT32               type;
+  EFI_PHYSICAL_ADDRESS physical_addr_start;
+  EFI_VIRTUAL_ADDRESS  virtual_addr_start;
+  UINT64               pages_num;
+  UINT64               attribute;
+} custom_memory_map;
+
+typedef struct _pml4_table {
+  unsigned char p;
+} pml4_table;
+
+BOOLEAN EFIAPI long_mode_enabled(UINTN cr0, UINTN cr4);
 UINTN EFIAPI level4_paging(void);
+
+UINTN EFIAPI save_memory_map(void);
 
 UINT8 EFIAPI trans_linear_addr(IN UINTN cr3);
 
--- paging.c
+++ paging.c
@@ -4,8 +4,9 @@
 #include <Library/PrintLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 
+#include <Library/BaseMemoryLib.h>
+
 #include "include/paging.h"
-#include "include/bits.h"
 
 UINT8 EFIAPI trans_linear_addr(IN UINTN cr3)
 {
@@ -18,59 +19,81 @@ UINT8 EFIAPI trans_linear_addr(IN UINTN cr3)
   return 0;
 }
 
-BOOLEAN EFIAPI long_mode_enabled(void)
+BOOLEAN EFIAPI long_mode_enabled(UINTN cr0, UINTN cr4)
 {
   UINT64 efer;
+  BOOLEAN status;
 
   efer = AsmReadMsr64(0xC0000080);
-  Print(L"efer = %x\r\n", efer);
-  if ((efer & 0x500) != 0x500) {
-    return 0;
+  status = 0;
+
+  if ((efer & BIT8) == 0 && (efer & BIT10) == 0) {
+    goto long_mode_disabled;
   }
 
-  return 1;
+  if ((cr0 & BIT31) == 0 && (cr4 & BIT5) == 0) {
+    goto long_mode_disabled;
+  }
+
+  status 1;
+long_mode_disabled:
+  return status;
 }
 
 // R0.PG = 1, CR4.PAE = 1, IA32_EFER.LME = 1, and //////////////////// CR4.LA57 = 0. 5 - level paging
 UINTN EFIAPI level4_paging(void)
 {
-  UINTN cr0, cr4, and_mask;
+  UINTN status, cr0, cr4;
   BOOLEAN long_mode;
 
+  status = 0;
   cr0 = AsmReadCr0();
   cr4 = AsmReadCr4();
 
-  long_mode = long_mode_enabled();
-  if (!long_mode) {
-    Print(L"long mode disabled\r\n");
-    return 0;
+  long_mode = long_mode_enabled(cr0, cr4);
+  if (long_mode) {
+    Print(L"long mode enabled\n");
+  } else {
+    Print(L"level4 paging disabled, long mode disabled\n");
+    goto disabled_level4;
   }
 
-  Print(L"CR0 = 0x%x\r\n", cr0);
-  and_mask = _BIT_LEFT(31);
-  if ((cr0 & and_mask) == 0) {
-    Print(L"CR0.PG = 0\r\n");
-    return 0;
-  } else {
-    Print(L"CR0.PG = 1\r\n");
-  }
-
-  Print(L"CR4 = 0x%x\r\n", cr4);
-  and_mask = _BIT_LEFT(5);
-  if ((cr4 & and_mask) == 0) {
-    Print(L"CR4.PAE = 0\r\n");
-    return 0;
-  } else {
-    Print(L"CR4.PAE = 1\r\n");
-  }
-
-  and_mask = _BIT_LEFT(12);
-  if ((cr4 & and_mask) == 1) {
-    Print(L"CR4.LA57 = 1\r\n");
-    return 0;
-  } else {
+  if ((cr4 & BIT12) == 0) {
     Print(L"CR4.LA57 = 0\r\n");
+  } else {
+    Print(L"level4 paging disabled, CR4.LA57 = 1\r\n");
+    goto disabled_level4;
   }
 
-  return 1;
+  status = 1;
+disabled_level4:
+  return status;
+}
+
+UINTN EFIAPI save_memory_map(void)
+{
+  EFI_MEMORY_DESCRIPTOR *memory_desc = NULL;
+  custom_memory_map *memory_map = NULL;
+  UINTN map_size, map_key, desc_size;
+  UINT32 desc_version;
+
+  gBS->AllocatePool(EfiRuntimeServicesCode, sizeof(custom_memory_map), (void **)&memory_map);
+
+  map_size = 0;
+  map_key = 0;
+  desc_size = sizeof(EFI_MEMORY_DESCRIPTOR);
+  ZeroMem(memory_map, sizeof(custom_memory_map));
+  desc_version = 0;
+
+  gBS->GetMemoryMap(&map_size, NULL, &map_key, &desc_size, &desc_version);
+  gBS->AllocatePool(EfiLoaderCode, map_size, (void **)&memory_desc);
+  gBS->GetMemoryMap(&map_size, memory_desc, &map_key, &desc_size, &desc_version);
+
+  Print(L"map_size = %llu, map_key = %llu, desc_size = %llu\n", map_size, map_key, desc_size);
+  Print(L"PhysicalAddress = 0x%lx\n", memory_desc->PhysicalStart);
+  Print(L"VirtualStart = 0x%lx\n", memory_desc->VirtualStart);
+  Print(L"NumberOfPages = 0x%lx\n", memory_desc->NumberOfPages);
+  Print(L"Attribute = %llu\n", memory_desc->Attribute);
+
+  return map_key;
 }
